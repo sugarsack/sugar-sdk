587512b1edea78fb48e1bd97d22680cad2a38afd
Bo Maryniuk <bo@suse.de>
2019-02-06 18:04:33 +0100

    Add documentation generator class

--- sugarsdk/gendoc.py
+++ sugarsdk/gendoc.py
@@ -5,6 +5,16 @@ Module documentation generator.
 Generates sort of online book out of the validated modules
 and their documentaiton.
 """
+from sugar.components.docman.docrnd import ModDocBase
+from sugar.components.docman.jinfilters import JinjaRstFilters
+
+
+class ModRSTDoc(ModDocBase):
+    """
+    Generate RST documentation.
+    """
+    filters = JinjaRstFilters()
+
 
 class ModuleDocumentationGenerator:
     """
