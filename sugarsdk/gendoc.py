3f243f554d65835213c31971cafd84634f3a9c1c
Bo Maryniuk <bo@suse.de>
2019-02-06 12:47:52 +0100

    Initial commit to the RST gendoc

---
+++ sugarsdk/gendoc.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+"""
+Module documentation generator.
+
+Generates sort of online book out of the validated modules
+and their documentaiton.
+"""
+
+class ModuleDocumentationGenerator:
+    """
+    Module documentaiton generator class.
+    """
+
+    def __init__(self, args):
+        self._args = args
+
+    def generate(self) -> None:
+        """
+        Generate a documentation.
+
+        :returns: None
+        """
