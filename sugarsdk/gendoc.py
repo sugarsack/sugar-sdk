b42e9f59c04c32bcb2c0c4bd29519b91e0f4275e
Bo Maryniuk <bo@suse.de>
2019-02-07 15:46:30 +0100

    Implement module TOC generator

--- sugarsdk/gendoc.py
+++ sugarsdk/gendoc.py
@@ -16,6 +16,31 @@ class ModRSTDoc(ModDocBase):
     filters = JinjaRstFilters()
 
 
+    def get_module_toc(self) -> str:
+        """
+        Get module TOC.
+
+        :return: rendered TOC
+        """
+
+        doc_header = self._docmap.get("doc", {}).get("module", {})
+        template = sugarsdk.utils.get_template("doc_m_idx_{}".format(self._mod_type))
+        func_list = self._docmap.get("doc", {}).get("tasks")
+
+        if func_list:
+            m_doc = type("mdoc", (), {
+                "uri": self._mod_uri, "author": doc_header.get("author", "N/A"),
+                "description": doc_header.get("synopsis", "N/A"), "summary": doc_header.get("summary"),
+                "version_added": doc_header.get("since_version", "N/A"),
+                "f_docs": ["doc_f_{}".format(_uri.replace(".", "_")) for _uri in func_list],
+            })
+            out = jinja2.Template(template).render(m_doc=m_doc, filters=self.filters, len=len)
+        else:
+            out = None
+
+        return out
+
+
 class ModuleDocumentationGenerator:
     """
     Module documentaiton generator class.
