7d7e627a89bf1de964dd27d196708b98087662c9
Bo Maryniuk <bo@suse.de>
2019-02-07 15:46:51 +0100

    Implement documentation index generator

--- sugarsdk/gendoc.py
+++ sugarsdk/gendoc.py
@@ -48,6 +48,8 @@ class ModuleDocumentationGenerator:
 
     def __init__(self, args):
         self._args = args
+        self.loader = SugarModuleLoader()  # We're not generating anything for the custom modules.
+        self.out = ConsoleMessages()
 
     def generate(self) -> None:
         """
@@ -55,3 +57,20 @@ class ModuleDocumentationGenerator:
 
         :returns: None
         """
+        self.out.info("Generating documentation for {} modules", "runner")
+        self.out.info("  - collecting TOC")
+        mod_toc = type("mod_toc", (), {"mod_runner": [], "mod_state": []})
+        for uri in sorted(self.loader.runners.map().keys()):
+            self.out.info("  - create module TOC for {}", uri)
+
+            toc_name = "doc_m_toc_{}".format(uri.replace(".", "_"))
+            mod_toc.mod_runner.append(toc_name)
+            mod_rst_doc = ModRSTDoc(uri, mod_type="runner")
+
+            self.out.info("  - write module TOC ({})", toc_name)
+            with sugar.utils.files.fopen("{}.rst".format(toc_name), "w") as toc_h:
+                toc_h.write(mod_rst_doc.get_module_toc())
+
+        self.out.info("  - write reference TOC ({})", toc_name)
+        with sugar.utils.files.fopen("modbook.rst", "w") as mbh:
+            mbh.write(jinja2.Template(sugarsdk.utils.get_template("doc_modbook")).render(mod_toc=mod_toc, len=len))
